a3005e5a09bfb8f855300612ee431bf878377af0
a-chaaa <99166941+a-chaaa@users.noreply.github.com>
2022-02-20 13:04:37 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -8,7 +8,7 @@ setTimeout(function () {
 		/*overlay: true,*/			//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
 		transition: 'fade',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
 		/*animation: 'kenburns',*/	//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
-		delay: 5000,			//次の画像を表示するまでの時間
+		delay: 3000,			//次の画像を表示するまでの時間
 		animationDuration: 20000,	//アニメーション間の引き継ぎタイミング。
     });
 }, 3000);	//3秒遅れてスライドショーがスタート
