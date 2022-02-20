6fa5d46fdacb604b7adabbd536e02f89ed705cfd
a-chaaa <99166941+a-chaaa@users.noreply.github.com>
2022-02-20 13:00:17 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -6,8 +6,8 @@ setTimeout(function () {
             { src: 'https://a-chaaa.github.io/rinakoteikoku/3.jpg' },	//3枚目の写真指定
         ],
 		/*overlay: true,*/			//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
-		transition: 'blur',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
-		animation: 'kenburns',	//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		transition: 'fade',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		/*animation: 'kenburns',*/	//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
 		delay: 5000,			//次の画像を表示するまでの時間
 		animationDuration: 20000,	//アニメーション間の引き継ぎタイミング。
     });
