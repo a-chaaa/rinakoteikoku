6e5be0762f3f48fac6e72dbf3064c4b315978395
a-chaaa <99166941+a-chaaa@users.noreply.github.com>
2022-02-20 12:47:17 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -1,9 +1,9 @@
 setTimeout(function () {
     $('#mainimg').vegas({
         slides: [
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/images/1.jpg' },	//1枚目の写真指定
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/images/2.jpg' },	//2枚目の写真指定
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/images/3.jpg' },	//3枚目の写真指定
+            { src: 'https://a-chaaa.github.io/rinakoteikoku/1.jpg' },	//1枚目の写真指定
+            { src: 'https://a-chaaa.github.io/rinakoteikoku/2.jpg' },	//2枚目の写真指定
+            { src: 'https://a-chaaa.github.io/rinakoteikoku/3.jpg' },	//3枚目の写真指定
         ],
 		/*overlay: true,*/			//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
 		transition: 'blur',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
