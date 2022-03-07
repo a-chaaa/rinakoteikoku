d3427702ce3962edaf9a725029c5e318564491bd
a-chaaa <99166941+a-chaaa@users.noreply.github.com>
2022-03-07 09:58:39 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -2,8 +2,8 @@ setTimeout(function () {
     $('#mainimg').vegas({
         slides: [
             { src: 'https://a-chaaa.github.io/rinakoteikoku/1.jpg' },	//1枚目の写真指定
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/2.jpg' },	//2枚目の写真指定
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/3.jpg' },	//3枚目の写真指定
+            { src: 'https://a-chaaa.github.io/rinakoteikoku/slide.png' },	//2枚目の写真指定
+            { src: 'https://a-chaaa.github.io/rinakoteikoku/シグナルとシグナレス.gif' },	//3枚目の写真指定
         ],
 		/*overlay: true,*/			//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
 		transition: 'fade',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
