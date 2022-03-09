923624d55e45fec4bb89e5e3988547f62c9fe653
a-chaaa <99166941+a-chaaa@users.noreply.github.com>
2022-03-09 11:31:27 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -2,8 +2,7 @@ setTimeout(function () {
     $('#mainimg').vegas({
         slides: [
             { src: 'https://a-chaaa.github.io/rinakoteikoku/ひょーし.png' },	//1枚目の写真指定
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/36.png' },	//2枚目の写真指定
-            { src: 'https://a-chaaa.github.io/rinakoteikoku/torekaaa.png' },	//3枚目の写真指定
+            { src: 'https://a-chaaa.github.io/rinakoteikoku/torekaaa.png' },	//2枚目の写真指定
 	    { src: 'https://a-chaaa.github.io/rinakoteikoku/ななな.png' },
         ],
 		/*overlay: true,*/			//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
